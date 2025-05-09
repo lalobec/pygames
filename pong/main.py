fc97e19c4de80a9ca190ad841530ef2201e22d93
lalobgc <lalobg.c@gmail.com>
2025-05-08 23:08:55 -0600

    Basic window creation

---
+++ pong/main.py
@@ -0,0 +1,18 @@
+import pygame, sys
+
+pygame.init()
+clock = pygame.time.Clock()
+
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Pong v0.1.0')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.flip()
+    clock.tick(60)

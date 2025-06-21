c1d8ff62c2bbeb77eeddebdbb2baff51eed2e79d
lalobgc <lalobg.c@gmail.com>
2025-06-21 13:52:48 -0600

    bouncing ball

--- pong/main.py
+++ pong/main.py
@@ -20,12 +20,23 @@ opponent = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
 
+ball_x_speed = 5
+ball_y_speed = 5
+
 while True:
     # Handling input
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    ball.x += ball_x_speed
+    ball.y += ball_y_speed
+
+    if ball.top <= 0 or ball.bottom >= screen_height:
+        ball_y_speed *= -1
+    if ball.left <= 0 or ball.right >= screen_width:
+        ball_x_speed *= -1
 
     # Visuals
     screen.fill(bg_color)
